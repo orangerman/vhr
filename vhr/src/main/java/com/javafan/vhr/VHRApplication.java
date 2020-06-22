fd3214de7222e42a2ce7e3a1145abe1a74d19363
javafan <1057600474@qq.com>
2020-06-22 18:14:22 +0800

    测试改名

--- vhr/src/main/java/com/javafan/vhr/VHRApplication.java
+++ vhr/src/main/java/com/javafan/vhr/VHRApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VhrApplication {
+public class VHRApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VhrApplication.class, args);
+        SpringApplication.run(VHRApplication.class, args);
     }
 
 }
