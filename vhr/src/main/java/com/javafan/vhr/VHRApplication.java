f9b9182e82dd098249f79547b7da025061bfc0eb
orangerman <44635711+orangerman@users.noreply.github.com>
2020-06-22 18:16:09 +0800

    Update VHRApplication.java

--- vhr/src/main/java/com/javafan/vhr/VHRApplication.java
+++ vhr/src/main/java/com/javafan/vhr/VHRApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VHRApplication {
 
+    /*
+    *测试
+    */
     public static void main(String[] args) {
         SpringApplication.run(VHRApplication.class, args);
     }
