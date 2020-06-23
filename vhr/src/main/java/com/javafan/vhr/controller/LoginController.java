b2b5340b9cce11a7df4b5f030392d519cf6bde83
javafan <1057600474@qq.com>
2020-06-23 09:13:38 +0800

    Spring Security基本搭建完成

--- vhr/src/main/java/com/javafan/vhr/controller/LoginController.java
+++ vhr/src/main/java/com/javafan/vhr/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class LoginController {
+
     @GetMapping("/login")
     public RespBean login() {
         return RespBean.error("尚未登陆，请登录");
